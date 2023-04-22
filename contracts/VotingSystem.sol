a4037619436da200a9de5690818d3489eea679ed
crown-of-napoleon <112432640+crown-of-napoleon@users.noreply.github.com>
2023-04-22 13:36:24 -0500

    Update VotingSystem.sol

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -1,4 +1,11 @@
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract VotingToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("VotingToken", "VOTE") {
+        _mint(msg.sender, initialSupply);
+    }
+}
 
 contract VotingSystem {
     struct Poll {
